b9f24bf75cb2520663528826e4afa38917e9a232
polinavirchenko <polinavirchenko@gmail.com>
2023-03-12 22:12:46 -0400

    SauceLab framework

---
+++ eclipse-workspace/Plhfgfd/src/MyFirstJavaProgram.java
@@ -0,0 +1,14 @@
+public class MyFirstJavaProgram {
+/* This is my first java program.
+* This will print 'Hello World' as the output
+*/
+	
+	public static int a;
+	public static String Is;
+	
+public static void main(String []args) {
+System.out.println("Hello\bWorld"); // prints Hello World
+System.out.println(Is);
+}
+
+}
